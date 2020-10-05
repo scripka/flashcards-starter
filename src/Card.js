467912a2a5e5837194e64d674e8b8f22afad965d
Olga Morgan <olga.morgan17@gmail.com>
2020-10-05 16:14:21 -0600

    Add card class and card test for parameters

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+class Card {
+  constructor(cardId, question, answers, correctAnswer) {
+    this.cardId = cardId;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+};
+
+
+
+
+
+
+
+
+module.exports = Card;
