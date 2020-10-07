5900c688b0767b642b769c615cef91c5bba164f0
Olga Morgan <olga.morgan17@gmail.com>
2020-10-07 00:40:50 -0600

    Add round/turn class/test and refactoring deck/card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,70 +6,57 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
+  let card1, card2, turn, anotherTurn;
+
+  beforeEach (function() {
+    card1 = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
+    card2 = new Card(2, 'How many colors in a rainbow?', ['nine', 'many', 'seven'], 'seven');
+    turn = new Turn('boo', card1);
+    anotherTurn = new Turn('woo', card2);
+  });
+
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should only accept a string answer from a user', function() {
-    const turn = new Turn('boo');
-    const newTurn = new Turn('woo');
-
     expect(turn.guess).to.equal('boo');
-    expect(turn.guess).to.be.a.string('boo');
+    expect(turn.guess).to.be.a('string');
 
-    expect(newTurn.guess).to.equal('woo');
-    expect(newTurn.guess).to.be.a.string('woo');
+    expect(anotherTurn.guess).to.equal('woo');
+    expect(anotherTurn.guess).to.be.a('string');
   });
 
   it('should play the current card', function() {
-    const card = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
-    const turn = new Turn('boo', card);
-
-    expect(turn.currentCard).to.deep.equal(card);
+    expect(turn.currentCard).to.equal(card1);
     expect(turn.currentCard).to.be.an.instanceof(Card);
 
   });
 
   it('should record the provided guess', function() {
-    const card = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
-    const turn = new Turn('boo', card);
-
     var turnGuessAnswer = turn.returnGuess();
 
-    expect(turnGuessAnswer).to.deep.equal(turn.guess);
+    expect(turnGuessAnswer).to.equal(turn.guess);
 
   });
 
   it('should record the current card', function() {
-    const card = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
-    const turn = new Turn('boo', card);
-
     var turnCurrentCard = turn.returnCard();
 
-    expect(turnCurrentCard).to.deep.equal(turn.currentCard);
+    expect(turnCurrentCard).to.equal(turn.currentCard);
   });
 
   it('should evaluate our guessed answers', function() {
-    let card = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
-    const turn = new Turn('boo', card);
-    const anotherTurn = new Turn('woo', card);
-
     expect(turn.evaluateGuess()).to.equal(true);
     expect(anotherTurn.evaluateGuess()).to.equal(false);
   });
 
   it('should give us a feedback', function() {
-    let card = new Card(1, "What is a scary word?", ['boo', 'bah', 'whah'], 'boo');
-    const turn = new Turn('boo', card);
-    const anotherTurn = new Turn('woo', card);
-
-    expect(turn.giveFeedback()).to.deep.equal('Correct!');
-    expect(anotherTurn.giveFeedback()).to.deep.equal('Incorrect!');
+    expect(turn.giveFeedback()).to.equal('Correct!');
+    expect(anotherTurn.giveFeedback()).to.equal('Incorrect!');
   });
 });
